0b4a2af3dad74255dda7a6ab5d356a6e1753f739
camilorosero11 <camilo.rosero@udea.edu.co>
2025-05-19 22:09:19 -0500

    Resolví conflictos con main

--- fecha.h
+++ fecha.h
@@ -9,6 +9,9 @@ private:
 
 public:
     Fecha(unsigned int _dia, unsigned int _mes, unsigned int _anio);
+    unsigned int diasEnMes(unsigned int mes, unsigned int anio)const;
+    void sumarDias(unsigned int diasHospedajes);
+    bool esFechaValida() const;
 };
 
 #endif // FECHA_H
